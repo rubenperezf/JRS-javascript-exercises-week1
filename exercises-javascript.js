ff5f99c4cec2e6214b4476e644aa82dd15e56156
Ruben Perez Feijoo <rubenperezfeijoo@Rubens-MacBook-Pro.local>
2019-10-14 07:49:27 -0400

    update 11

--- exercises-javascript.js
+++ exercises-javascript.js
@@ -89,7 +89,7 @@ function mean(array) {
     var sum=0;
     for(let i =0; i<array.length;i++) {
         sum = sum + array[i];
-    } return sum;
+    } return sum/array.length;
 }
 console.log(mean(arrayRuben))
 
