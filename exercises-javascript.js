3a725009fadcabcce917761d55508961e78a78e3
Ruben Perez Feijoo <rubenperezfeijoo@Rubens-MBP.attlocal.net>
2019-10-19 10:25:49 -0400

    exercises until 18 edited

--- exercises-javascript.js
+++ exercises-javascript.js
@@ -1,40 +1,53 @@
 //1) Write a program that displays the result of 5+6+7+8+9+10.
+
 function addNumbers() {
   console.log(5+6+7+8+9+10);
 }
+
 addNumbers()
 
 //2) Write a function that returns the perimeter of a rectangle with a width of 5 and a height of 8.
+
 function rectanglePerimeter() {
 return((5*9) + " is the perimeter of this rectangle.");
 }
+
 console.log(rectanglePerimeter());
 
 //3)Write a new function that returns the perimeter of a rectangle with the width and height being specified as arguments to the function.
+
 function recPerimeter(width, height) {
 return(width*height + " is the perimeter of this rectangle.");
 }
+
 console.log(recPerimeter(5,8));
 
 //4) Write a function that converts fahrenheit, given as an argument, to celcius.
+
 function getCelcius(fahrenheit) {
 return(fahrenheit +" Fahrenghit are " +(fahrenheit-32)*5/9) + " in Celcius";
 }
+
 console.log(getCelcius(234));
 
 //5) Write a function that converts celcius, given as an argumen, to fahrenheit.
+
 function getFahrenheit(celcius) {
 return(celcius + " Celcius are " + (celcius*9/5+32) + " in Fahrenheit");
 }
+
 console.log(getFahrenheit(25));
 
 //6) Write a function that calculates tip with 2 arguments, the bill and the tip percentage. Use the build-in JS toFixed() method to round the answer to two decimal places.
+
 function getTip (bill, percentage) {
 return ("You have to pay "+ (bill*(percentage/100) +bill).toFixed(2));
 }
+
 console.log(getTip(100,10));
 
 //7) Write a function that takes a number an an argument and returns the sum of each individual digit. So an input of 998 would return 26 (because 9 + 9 + 8) is 26.
+
 function sumOfDigits(num){
   var sum = 0;
   var newString = num.toString();
@@ -44,26 +57,28 @@ function sumOfDigits(num){
   
   return sum;
 }
+
 console.log(sumOfDigits(22))
 
-
-
-
-
 //8) Write a function that takes an argument in seconds and specifies the equivalent number of years.
+
 function getYears (seconds) {
 return (seconds/31536000).toFixed(5) + " number of years";
 }
+
 console.log(getYears(1000));
 
 //8) Write the same function above, but that takes an input from the built-in browser function, prompt().
+
 function getYears () {
   var seconds = prompt("how many second?");
   return((seconds/31536000).toFixed(5) + " number of years");
   }
+
 console.log(getYears());
 
 //9) Write a function that returns the current date and time.
+
 function newDate () {
   var today = new Date();
   var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
@@ -71,6 +86,7 @@ function newDate () {
   var dateTime = date+' '+time;
   return dateTime;
   } 
+
   console.log(newDate());
 
 
@@ -81,6 +97,7 @@ function subtractDays (days) {
   date.setDate(date.getDate()-days);
   return date
 }
+
 console.log(subtractDays(33))
 
 //11) Write a function that returns the mean of an array of numbers.
@@ -95,20 +112,18 @@ function meanArray (array) {
 var arrayRuben =[1,6,7,8];
 console.log(mean(arrayRuben))
 
-
-
-
 //12) Write a function that randomly generates a number between 1 and 12, and returns the name of the corresponding month.
 
 function getMonth() {
-const monthNames = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"];
-var number = (Math.floor(Math.random()*12));
-return monthNames[number];
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var number = (Math.floor(Math.random()*12));
+  return monthNames[number];
 }
+
 console.log(getMonth());
 
 //13) Write a function that takes three numbers as arguments, and returns the same 3 numbers in increasing order (more specifically, non-decreasing order if multiple numbers are the same).
+
 function increaseNumbers(num1,num2, num3) {
   if(num1>num2 &&num1>num3 && num2>num3) {
       return ([num1,num2,num3])
@@ -125,25 +140,24 @@ function increaseNumbers(num1,num2, num3) {
   }
 }
 
-
 console.log(increaseNumbers(90,7,100));
+
 //14) Write a function that returns the appropriate boolean value if a given string argument is a palindrome, where the string is the same when reversed. Examples of palindromes are "racecar" and "3003".
 //Ex: isPalindrome("racecar") returns true. isPalindrome("racecars") returns false.
 
 function isPalindrome (string) {
-  for(i=0; i<string.length;i++) {
-      if(string[i]=== string[string.length-i-1]) {
-      } else {
-          return false;
-      }
-  } return true;
+  var halfString = Math.floor(string.length / 2);
+  for (var i = 0; i < halfString; i++)
+    if (string[i] !== string[string.length - i - 1])
+      return false;
+  return true;
 }
-console.log(isPalindrome("racertrtrcar"));
 
-
+console.log(isPalindrome("racertrtrecar"));
 
 //15) Write a function that returns true when two inputs are equivalent when using coercion, but not equivalent without coercion.
 //Ex: isCoerced(5,"5") returns true. isCoerced(5,5) returns false. isCoerced(4,"shoe") returns false.
+
 function isCoerced(a,b) {
   if (a===b) {
     return false
@@ -152,12 +166,14 @@ function isCoerced(a,b) {
   } else if (a !=b) {
     return false
   }
-  }
+}
+
   console.log(isCoerced(5,"5"));
   console.log(isCoerced(5,5));
   console.log(isCoerced(4,"shoe"));
   
-  //16) Write a function called isEven() that returns true if a given argument is even.
+//16) Write a function called isEven() that returns true if a given argument is even.
+
   function isEven(number) {
     if(number%2 ===0) {
         return true 
@@ -165,14 +181,17 @@ function isCoerced(a,b) {
         return false;
     }
 }
+
 console.log(isEven(44));
 
 function isEven2(number) {
   return number%2 ===0
 }
+
 console.log(isEven2(45));
 
 //17) Write a function called isOdd() that returns true if a given argument is odd. Do this by only using the isEven() function you wrote previously.
+
 function isOdd(number) {
   if(isEven(number)===true){
       return false;
@@ -180,13 +199,17 @@ function isOdd(number) {
       return true;
   }
 }
+
 console.log(isOdd(55));
+
 function isOdd2(number) {
-return !isEven(number)
+  return !isEven(number)
 }
+
 console.log(isOdd2(54));
 
 //18) Write a function that returns true if a given argument is a multiple of 3. Examples of multiples of 3 are 0,3,6,9 ...
+
 function multipleOf3 (number) {
   if(number%3===0) {
     return true;
@@ -194,7 +217,15 @@ function multipleOf3 (number) {
     return false;
   }
 }
+
 console.log(multipleOf3(10));
+
+function multipleOf32 (number) {
+  return number%3===0
+}
+
+console.log(multipleOf32(10));
+
 //19) Write a function that takes two arguments. Check if the first argument is the multiple of the second argument. Return the appropriate boolean.
 //Ex. checkMultiple(20,5) returns true. checkMultiple(21,5) returns false.
 function multipleNumber(num1,num2) {
